d13a3bc18e03777cdbc34a0bdda509b9a48ad71c
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-15 22:48:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -220,13 +220,13 @@ if pestañas == "Modelos con una variable":
        modelo_musculo_dt_simplified = DecisionTreeRegressor(max_depth=4)  # Ajusta el valor de max_depth según sea necesario
        modelo_musculo_dt_simplified.fit(X, y)
 
-       # Generar el diagrama del árbol de decisión simplificado
+       # Generar el diagrama del árbol de decisión simplificado con un tamaño de letra más grande
        figur = plt.figure(figsize=(40, 20))
-       plt.rc('font', size=25)  # Ajusta el tamaño de fuente aquí
-       plot_tree(modelo_musculo_dt_simplified, filled=True, feature_names=X.columns)
+       plt.rc('font', size=12)  # Ajusta el tamaño de fuente aquí
+       plot_tree(modelo_musculo_dt_simplified, filled=True, feature_names=X.columns, fontsize=14)  # Ajusta el tamaño de la letra aquí
        plt.title("Árbol de Decisión Simplificado para Musculo (kg) vs. PPantorrilla (cm)", fontsize=30)  # Ajusta el tamaño de fuente del título aquí
 
-
+       # Mostrar la figura en Streamlit
        st.pyplot(figur)
 
        ####################
