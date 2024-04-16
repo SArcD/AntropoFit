d932e6b9fef0a39c5e41d8ccb668e73dee9e5f50
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-15 23:08:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ if pestañas == "Modelos con una variable":
        # Generar el diagrama del árbol de decisión simplificado con un tamaño de letra más grande
        figur = plt.figure(figsize=(40, 10), dpi=300)
        plt.rc('font', size=12)  # Ajusta el tamaño de fuente aquí
-       plot_tree(modelo_musculo_dt_simplified, filled=True, feature_names=X.columns, fontsize=14)  # Ajusta el tamaño de la letra aquí
+       plot_tree(modelo_musculo_dt_simplified, filled=True, feature_names=X.columns, fontsize=12)  # Ajusta el tamaño de la letra aquí
        plt.title("Árbol de Decisión Simplificado para Musculo (kg) vs. PPantorrilla (cm)", fontsize=30)  # Ajusta el tamaño de fuente del título aquí
 
        # Mostrar la figura en Streamlit
