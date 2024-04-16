d2c65b6a16857b3d6436b7350ae8da16d959ed90
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-16 14:46:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1229,6 +1229,7 @@ elif pestañas == "Calculadora":
                nombre_archivo_excel = "df_con_predicciones.xlsx"
                df.to_excel(nombre_archivo_excel, index=False)
                st.write(f"Se ha guardado el DataFrame actualizado en {nombre_archivo_excel}")
+               df_2=df
                df_2
                clasificar_filas(df_2)
                df_2
